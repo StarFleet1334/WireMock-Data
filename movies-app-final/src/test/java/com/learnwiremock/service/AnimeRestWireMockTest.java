a5ba6a0e8c2d961d15189d281111b76676c71204
IliaLataria01 <latariailia6@gmail.com>
2024-06-27 02:52:43 +0400

    Did POST,UPDATE and 404-Error case tests

--- movies-app-final/src/test/java/com/learnwiremock/service/AnimeRestWireMockTest.java
+++ movies-app-final/src/test/java/com/learnwiremock/service/AnimeRestWireMockTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,14 +26,15 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static com.learnwiremock.endpoints.UtilEndpoints.GET_ALL_ANIME;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.learnwiremock.endpoints.UtilEndpoints.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(WireMockExtension.class)
 public class AnimeRestWireMockTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnimeRestWireMockTest.class);
 
     AnimeRestClient animeRestClient;
     WebClient webClient;
@@ -117,7 +120,33 @@ public class AnimeRestWireMockTest {
     }
 
     @Test
-    void addNewAnime() {
+    void retrieveAnimeById404Response() throws InterruptedException {
+        stubFor(get(urlPathMatching("/animes/[0-9]+")).willReturn(
+                aResponse()
+                        .withStatus(HttpStatus.NOT_FOUND.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("404-error.json")));
+        Integer animeId = 100;
+        Mono<Anime> animeMono = animeRestClient.getAnimeById(animeId);
+        AtomicReference<String> title = new AtomicReference<>("");
+        AtomicReference<Integer> id = new AtomicReference<>(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        animeMono.subscribe(
+                anime -> {
+                    title.set(anime.getTitle());
+                    id.set(anime.getId());
+                    latch.countDown();
+                },
+                error -> latch.countDown()
+        );
+
+        latch.await();
+        LOGGER.info("Anime id: " + animeId + " title: " + title.get() + " id: " + id.get());
+    }
+
+    @Test
+    void addNewAnime() throws InterruptedException {
         Anime newAnime = Anime.builder()
                 .title("Kavawaki")
                 .rating(9.2)
@@ -125,21 +154,86 @@ public class AnimeRestWireMockTest {
                 .description("It is long story")
                 .build();
 
-        Mono<Anime> savedAnimeMono = animeRestClient.postNewAnime(newAnime);
 
+        stubFor(post(urlPathEqualTo(POST_ANIME.getPath())).willReturn(
+                aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("post-anime.json")));
+
+        Mono<Anime> savedAnimeMono = animeRestClient.postNewAnime(newAnime);
+        AtomicReference<String> title = new AtomicReference<>("");
+        CountDownLatch latch = new CountDownLatch(1);
+
+        savedAnimeMono.subscribe(
+                anime -> {
+                    title.set(anime.getTitle());
+                    latch.countDown();
+                },
+                error -> latch.countDown()
+        );
+
+        latch.await();
+        assertEquals(newAnime.getTitle(), title.get(), "The anime title should be " + newAnime.getTitle());
     }
 
     @Test
-    void updateAnime() {
-        Anime updatedAnime = new Anime(3, "One Piece (Updated)", 9, "Monkey D. Luffy", "The quest continues for the ultimate treasure, One Piece.");
+    void updateAnime() throws InterruptedException {
+        Anime updatedAnime = new Anime(3, "One Piece", 9, "King", "The quest continues for the ultimate treasure, One Piece.");
+
+        stubFor(put(urlPathMatching("/animes/[0-9]+"))
+                        .withRequestBody(matchingJsonPath("$.maincharacter",containing("King")))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("update-anime.json")));
+
+
         Mono<Anime> updatedAnimeMono = animeRestClient.updateExistingAnime(3, updatedAnime);
 
+        AtomicReference<String> mainCharacter = new AtomicReference<>("");
+        CountDownLatch latch = new CountDownLatch(1);
+
+        updatedAnimeMono.subscribe(
+                anime -> {
+                    mainCharacter.set(anime.getMaincharacter());
+                    latch.countDown();
+                },
+                error -> latch.countDown()
+        );
+
+        latch.await();
+        assertEquals(mainCharacter.get(), "Monkey D. Luffy - King", "The maincharacter should be " + updatedAnime.getMaincharacter());
+
     }
 
     @Test
-    void deleteAnime() {
-        Mono<String> deleteResponseMono = animeRestClient.deleteAnimeById(4);
+    void deleteAnime() throws InterruptedException {
+        int id = 4;
+        String deleteMessage = "Deleted anime with id: " + id;
 
+        stubFor(delete(urlPathEqualTo("/animes/" + id))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(deleteMessage)));
+
+        Mono<String> deleteResponseMono = animeRestClient.deleteAnimeById(id);
+
+        // Setup for asynchronous result checking
+        AtomicReference<String> result = new AtomicReference<>("");
+        CountDownLatch latch = new CountDownLatch(1);
+
+        deleteResponseMono.subscribe(
+                response -> {
+                    result.set(response);
+                    latch.countDown();
+                },
+                error -> latch.countDown()
+        );
+
+        latch.await();  // Ensuring the Mono completes before assertion
+        assertEquals(deleteMessage, result.get(), "Response message should match expected deletion message");
     }
 
     @AfterEach
