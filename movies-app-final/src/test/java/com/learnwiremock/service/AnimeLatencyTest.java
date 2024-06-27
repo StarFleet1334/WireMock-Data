c19f3078bb74224a7f465a9fddaab1a9843a6ed8
IliaLataria01 <latariailia6@gmail.com>
2024-06-27 13:29:41 +0400

    Finish cofiguration of webclient with timeout option

---
+++ movies-app-final/src/test/java/com/learnwiremock/service/AnimeLatencyTest.java
@@ -0,0 +1,62 @@
+package com.learnwiremock.service;
+
+import com.github.jenspiegsa.wiremockextension.ConfigureWireMock;
+import com.github.jenspiegsa.wiremockextension.InjectServer;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@ExtendWith(WireMockExtension.class)
+
+public class AnimeLatencyTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnimeRestWireMockTest.class);
+
+    AnimeRestClient animeRestClient;
+    WebClient webClient;
+
+    @InjectServer
+    WireMockServer wireMockServer;
+
+    @ConfigureWireMock
+    Options options = wireMockConfig().
+            port(8088)
+            // This notifier prints in the console all the necessary information
+            .notifier(new ConsoleNotifier(true))
+            .extensions(new ResponseTemplateTransformer(true));
+
+    // This one takes read/write timeout
+    TcpClient tcpClient = TcpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,5000)
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5))
+                        .addHandlerLast(new WriteTimeoutHandler(5));
+            });
+    // providing timeout 5 seconds
+
+    @BeforeEach
+    void setUp() {
+        int port = wireMockServer.port();
+        String baseUrl = String.format("http://localhost:%s/", port);
+        System.out.println("baseUrl : " + baseUrl);
+        // we have to inject timeout instance which is TcpClient
+        webClient = WebClient.builder().clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .baseUrl(baseUrl).build();
+        animeRestClient = new AnimeRestClient(webClient);
+
+    }
+}
